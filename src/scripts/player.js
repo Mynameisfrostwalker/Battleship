21ac5cf906ccb699b0a7d90a6673bce4ccccc6ee
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-04 07:54:06 +0100

    created player module

---
+++ src/scripts/player.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+/**
+ * @module player
+ */
+
+/**
+ * creates Players
+ * @param {string} player
+ * @return {Object}
+ */
+const createPlayer = (player = "human") => {
+  let turn = true;
+  let attack;
+
+  const getTurn = () => {
+    return turn;
+  };
+
+  const setTurn = (value) => {
+    turn = value;
+  };
+
+  if (player === "human") {
+    attack = (board, coords) => {
+      board.receiveAttack(coords);
+    };
+  } else if (player === "AI") {
+    attack = (board) => {
+      const available = [];
+      for (let i = 0; i < board.gameboard.length; i++) {
+        for (let j = 0; j < board.gameboard[i].length; j++) {
+          if (board.gameboard[i][j] === null || !board.gameboard[i][j].isHit) {
+            available.push([i, j]);
+          }
+        }
+      }
+      const y = Math.floor(Math.random() * available.length);
+      board.receiveAttack(available[y]);
+    };
+  }
+
+  return { getTurn, setTurn, attack };
+};
+
+export { createPlayer };
---
+++ src/tests/player.test.js
@@ -0,0 +1,71 @@
+import { createPlayer } from "../scripts/player";
+import { createGameboard } from "../scripts/gameboard";
+
+test("can get player turn", () => {
+  const player1 = createPlayer("human");
+  expect(player1.getTurn()).toBe(true);
+});
+
+test("can get player turn", () => {
+  const player1 = createPlayer("human");
+  player1.setTurn(false);
+  expect(player1.getTurn()).toBe(false);
+});
+
+test("player can attack enemy", () => {
+  const player1 = createPlayer("human");
+  const enemyBoard = createGameboard();
+  player1.attack(enemyBoard, [2, 1]);
+  expect(enemyBoard.gameboard[2][1]).toBe("miss");
+});
+
+test("AI can attack enemy", () => {
+  const player1 = createPlayer("AI");
+  const enemyBoard = createGameboard();
+  player1.attack(enemyBoard);
+  const boolean = enemyBoard.gameboard.reduce((accum, curr) => {
+    const value = curr.every((item) => item === null);
+    return accum && value;
+  }, true);
+  expect(boolean).toBe(false);
+});
+
+test("can not hit same coordinate twice", () => {
+  let count = 0;
+  const player1 = createPlayer("AI");
+  const enemyBoard = createGameboard();
+  const mathRandomSpy = jest.spyOn(Math, "random");
+  mathRandomSpy.mockImplementation(() => 0.5);
+  player1.attack(enemyBoard);
+  enemyBoard.gameboard.forEach((curr) => {
+    curr.forEach((item) => {
+      if (item !== null) {
+        count++;
+      }
+    });
+  });
+  player1.attack(enemyBoard);
+  expect(count).toBe(1);
+  player1.attack(enemyBoard);
+  enemyBoard.gameboard.forEach((curr) => {
+    curr.forEach((item) => {
+      if (item !== null) {
+        count++;
+      }
+    });
+  });
+  player1.attack(enemyBoard);
+  expect(count).toBe(2);
+  mathRandomSpy.mockRestore();
+});
+
+test("AI can hit ships", () => {
+  const player1 = createPlayer("AI");
+  const enemyBoard = createGameboard();
+  enemyBoard.placeShip("battleship", [0, 0]);
+  const mathRandomSpy = jest.spyOn(Math, "random");
+  mathRandomSpy.mockImplementation(() => 0);
+  player1.attack(enemyBoard);
+  expect(enemyBoard.gameboard[0][0].isHit).toBe(true);
+  mathRandomSpy.mockRestore();
+});
