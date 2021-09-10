9a4fbd28b377e5a042bb3145f4e98cfe192114c4
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2021-09-10 08:04:12 +0100

    called pubsub subscriptions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,11 @@
 import "../styles/reset.css";
 import "../styles/main.css";
+import { displayGame } from "./gameDisplay";
+import { game } from "./gameLoop";
+import { gameboardDisplay } from "./gameboardDisplay";
+import { subscribe } from "./pubsub";
+import { tdEvent, removeTdEvent, newGameEvent } from "./events";
+import { displayGameOver } from "./gameDisplay";
 
 // @ts-check
 
@@ -7,3 +13,17 @@ import "../styles/main.css";
  * @fileoverview Index.js is the rrot file of this project.
  * @author Frostwalker
  */
+
+displayGame();
+
+subscribe("cellClick", tdEvent);
+
+subscribe("removeCellClick", removeTdEvent);
+
+subscribe("displayGameboard", gameboardDisplay);
+
+subscribe("gameOver", displayGameOver);
+
+subscribe("newGame", newGameEvent);
+
+game();
